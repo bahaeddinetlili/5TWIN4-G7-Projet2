d3298d20faa6156cc2f4050589bd347a7ae75d26
skander_mazgar <skander.mazgar@esprit.tn>
2023-11-16 21:25:06 +0100

    Reset Main

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.kaddem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
